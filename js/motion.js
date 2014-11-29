c78e2a03cc7a5a1f67817eaced3c51a674e46cf6
SallyLin <sallylin94@gmail.com>
2014-11-30 02:56:01 +0800

    add more content

--- js/motion.js
+++ js/motion.js
@@ -183,62 +183,41 @@ var main = function(){
 
 	})
 
-	$('.headlight1').click(function(){
+	$('.headlight.1').click(function(){
 		var currentHeadlight = $('.active-headlight')
-		var nextHeadlight = $('.headlight1')
-		
-		var currentTitle = $('.active-title')
-		var nextTitle = $('.title1')
-
-		var currentPublish = $('.active-publish')
-		var nextPublish = $('.publish1')
-
-		var currentStorySummary = $('.active-storySummary')
-		var nextStorySummary = $('.storySummary1')
-
-		var currentStory = $('.active-story')
-		var nexxtStory = $('.story1')
+		var nextHeadlight = $('.headlight.1')
+		var currentArticle = $('.active-article')
+		var nextArticle = $('.article1_1')
 
 		currentHeadlight.removeClass('active-headlight')
 		nextHeadlight.addClass('active-headlight')
-		currentTitle.removeClass('active-title')
-		nextTitle.addClass('active-title')
-		currentPublish.removeClass('active-publish')
-		nextPublish.addClass('active-publish')
-		currentStorySummary.removeClass('active-storySummary')
-		nextStorySummary.addClass('active-storySummary')
-		currentStory.removeClass('active-story')
-		nexxtStory.addClass('active-story')
+		currentArticle.removeClass('active-article')
+		nextArticle.addClass('active-article')
 	})
 
-	$('.headlight2').click(function(){
+	$('.headlight.2').click(function(){
 		var currentHeadlight = $('.active-headlight')
-		var nextHeadlight = $('.headlight2')
-		
-		var currentTitle = $('.active-title')
-		var nextTitle = $('.title2')
-
-		var currentPublish = $('.active-publish')
-		var nextPublish = $('.publish2')
-
-		var currentStorySummary = $('.active-storySummary')
-		var nextStorySummary = $('.storySummary2')
-
-		var currentStory = $('.active-story')
-		var nexxtStory = $('.story2')
+		var nextHeadlight = $('.headlight.2')
+		var currentArticle = $('.active-article')
+		var nextArticle = $('.article2_1')
 
 		currentHeadlight.removeClass('active-headlight')
 		nextHeadlight.addClass('active-headlight')
-		currentTitle.removeClass('active-title')
-		nextTitle.addClass('active-title')
-		currentPublish.removeClass('active-publish')
-		nextPublish.addClass('active-publish')
-		currentStorySummary.removeClass('active-storySummary')
-		nextStorySummary.addClass('active-storySummary')
-		currentStory.removeClass('active-story')
-		nexxtStory.addClass('active-story')
+		currentArticle.removeClass('active-article')
+		nextArticle.addClass('active-article')
 	})
 
+	$('.headlight.3').click(function(){
+		var currentHeadlight = $('.active-headlight')
+		var nextHeadlight = $('.headlight.3')
+		var currentArticle = $('.active-article')
+		var nextArticle = $('.article3_1')
+
+		currentHeadlight.removeClass('active-headlight')
+		nextHeadlight.addClass('active-headlight')
+		currentArticle.removeClass('active-article')
+		nextArticle.addClass('active-article')
+	})
 
 	$('.topic').hover(function(){
 		$(this).css('border-bottom','2px solid')},function(){$(this).css('border-bottom','0px')}
@@ -251,7 +230,6 @@ var main = function(){
 	$('.trending').hover(function(){
 		$(this).css('border-bottom','2px solid')},function(){$(this).css('border-bottom','0px')}
 	)
-
 
 }
 
